78b7750df6bd17bf3511d5280a85eaf5ac64bdc8
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-09-12 13:41:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function threeSum(arr, target) {
 // write your code here
+	let n = arr.length;
+    let len = 3;
+    let minDiff=Number.MAX_VALUE;
+    let ans = 0;
+    for(let i=0;i<=n-len;i++){
+      let sum=0;
+      let halfAns= arr.slice(i,i+len);
+      for(let j of halfAns){
+        sum = sum +j;
+      }
+      
+      let diff= Math.abs(sum -target);
+      if(diff < minDiff ){
+        minDiff = diff;
+        ans = sum;
+      }
+
+      
+    }
+
+   return(ans);
   
 }
 
